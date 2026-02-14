18a7a2791594c65deb8532e10f3c7de37c0b82c9
max-matinpalo <matinpalo.m@gmail.com>
2026-02-14 19:21:23 +0200

    Update

--- src/devSounds.js
+++ src/devSounds.js
@@ -14,14 +14,16 @@ export async function setupDevSounds(app) {
 	const err = '/System/Library/Sounds/Submarine.aiff';
 
 
-	// Test fix, that sound not plays for static served 😃
-	const ct = String(reply.getHeader('content-type') || '').toLowerCase();
-	if (ct && ct.includes('application/json')) return;
 
 
 	const play = file => exec(`afplay "${file}" >/dev/null 2>&1 &`);
 
 	app.addHook('onResponse', async (req, reply) => {
+
+		// Test fix, that sound not plays for static served 😃
+		const ct = String(reply.getHeader('content-type') || '').toLowerCase();
+		if (ct && ct.includes('application/json')) return;
+
 		const code = reply.statusCode;
 		if (code < 400) play(ok);
 		else play(err);
